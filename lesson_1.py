f2a8cf6231811dd4886429ec1540f9f56172cecb
Cara Colker-Eybel <ccolkereybel@gmail.com>
2025-07-28 16:20:02 -0400

    updates to lesson 1

--- lesson_1.py
+++ lesson_1.py
@@ -1,25 +1,4 @@
-numbers = [1, 2, 3, 4]
-numbers[0] = numbers[0] + 1
-numbers[1] = numbers[1] + 1
-numbers[2] = numbers[2] + 1
-numbers[3] = numbers[3] + 1
+colors = ["red", "green", "blue"]
 
-
-print(numbers)
-
-dict1 = {
-    'apple': 'Produce',
-    'carrot': 'Produce',
-    'pear': 'Produce',
-    'broccoli': 'Produce',
-}
-
-dict1['apple'] = 'Fruit'
-dict1['carrot'] = 'Vegetable'
-dict1['pear'] = 'Fruit'
-dict1['broccoli'] = 'Vegetable'
-dict1['watermelon'] = 'Fruit'
-
-del dict1['carrot']
-
-print(dict1)
+for idx, color in enumerate(colors):
+    print(f'Color {color} at index {idx}')
